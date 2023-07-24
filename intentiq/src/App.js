93c1967701e3c62e8b012ceb7a26e383c8f5bbaa
elyashivmn0@gmail.com <elyashivmn0@gmail.com>
2023-07-24 16:48:03 +0300

    add material ui

--- intentiq/src/App.js
+++ intentiq/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import data from './mockData.json';
+
 import {
   Table,
   TableBody,
@@ -32,23 +33,22 @@ function App() {
   const [tableData, setTableData] = useState(data);
   const [displayedData, setDisplayedData] = useState([]);
   const [sortOrder, setSortOrder] = React.useState("asc");
-
-  // useEffect(() => {
-  //   const sorted = tableData.sort((a, b) => {
-  //     if (sortOrder === "asc") {
-  //       return new Date(a.startDate) - new Date(b.startDate);
-  //     } else {
-  //       return new Date(b.startDate) - new Date(a.startDate);
-  //     }});
-  //   setDisplayedData( sorted.slice(page * rowsPerPage, (page + 1) * rowsPerPage));
-  // }, [tableData, page, sortOrder]);
+  useEffect(() => {
+    const sorted = tableData.sort((a, b) => {
+      if (sortOrder === "asc") {
+        return new Date(a.startDate) - new Date(b.startDate);
+      } else {
+        return new Date(b.startDate) - new Date(a.startDate);
+      }});
+    setDisplayedData( sorted.slice(page * rowsPerPage, (page + 1) * rowsPerPage));
+  }, [tableData, page, sortOrder]);
   
   const handleSortOrderChange = () => {
-    // setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
+    setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
   };
 
   const handleChangePage = (event, newPage) => {
-    //  setPage(newPage);
+     setPage(newPage);
   };
 
   const table = useMemo(() =>(
